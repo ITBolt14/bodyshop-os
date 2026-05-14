ed19aa74c7499755f495e3b7b5d92e76237acf32
bolt1 <bolt1@itlegends.co.za>
2026-05-14 16:12:09 +0200

    Pushed the last few files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          50: '#f0f4ff',
+          100: '#e0e9ff',
+          200: '#c7d7fe',
+          300: '#a5bbfc',
+          400: '#8098f9',
+          500: '#6175f4',
+          600: '#4a52e8',
+          700: '#3b40ce',
+          800: '#3137a6',
+          900: '#2e3383',
+        }
+      }
+    },
   },
   plugins: [],
 }
-
